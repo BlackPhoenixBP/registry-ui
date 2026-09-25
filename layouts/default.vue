58a1bff5de6e710f76b838922c640b79d4bae4f5
Marlon Bitterhoff <marlon.bitterhoff1@gmail.com>
2026-09-25 23:11:14 +0200

    Complete Tailwind dark theme migration

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,9 +17,9 @@ useHead({
 </script>
 
 <template>
-  <div class="flex flex-col h-screen bg-gray-50">
-    <header class="bg-white border-b border-gray-200 p-4 flex items-center gap-2">
-      <img class="h-12" src="/logo.png" alt="logo" />
+  <div class="flex flex-col h-screen bg-gray-50 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
+    <header class="bg-white border-b border-gray-200 p-4 flex items-center gap-2 dark:bg-gray-800 dark:border-gray-700">
+      <img class="h-12 dark:invert dark:hue-rotate-180 dark:mix-blend-lighten" src="/logo.png" alt="logo" />
       <span class="font-bold text-lg">{{ config.public.registryTitle }}</span>
       <ThemeToggle />
     </header>
